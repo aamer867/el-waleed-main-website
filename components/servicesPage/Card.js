4094093772283fdcd386d1eb08f001ae74842e01
mo <mohamedaamer867@gmail.com>
2026-06-16 22:49:38 +0200

    reuptade the upload

--- components/servicesPage/Card.js
+++ components/servicesPage/Card.js
@@ -51,8 +51,6 @@ export default function Card({ header, description, imageUrl }) {
                 </li>
               ))}
             </ul>
-
-            
           </div>
         </div>
       </div>
